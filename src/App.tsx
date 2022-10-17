c29a80db28438fb78288e816a9002c60ddaa5854
micael.costa <micael.costa@celfinet.com>
2022-10-17 17:50:40 +0100

    change some code

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { sum } from './calc';
 const App = ({ onClick }: { onClick: () => void }) => {
 	return (
 		<>
-			<div>{`App xpto1 ${sum(4, 5)}`}</div>
+			<div>{`App ${sum(4, 5)}`}</div>
 			<button onClick={onClick}>Teste</button>
 		</>
 	)
