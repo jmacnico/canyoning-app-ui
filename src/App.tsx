13b42edae9a3dea6cd11c0874894d3118e1397d6
micael.costa <micael.costa@celfinet.com>
2022-10-17 16:57:08 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { sum } from './calc';
 const App = ({ onClick }: { onClick: () => void }) => {
 	return (
 		<>
-			<div>{`App ${sum(4, 5)}`}</div>
+			<div>{`App xpto ${sum(4, 5)}`}</div>
 			<button onClick={onClick}>Teste</button>
 		</>
 	)
