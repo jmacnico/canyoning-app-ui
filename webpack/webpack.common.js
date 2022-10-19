85ef7fe06bed2f68eea6adce4aa1d1ec24e7896c
micael.costa <micael.costa@celfinet.com>
2022-10-19 13:33:04 +0100

    feat: add docker configurations

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,40 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugion = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugion = require('html-webpack-plugin')
 
 module.exports = {
-	entry: path.resolve(__dirname, '..', './src/index.tsx'),
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	optimization: {
-		noEmitOnErrors: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'ts-loader'
-					}
-				]
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			}
-		]
-	},
-	output: {
-		path: path.resolve(__dirname, '..', './build'),
-		filename: 'bundle.js',
-		clean: true
-	},
-	mode: 'development',
-	plugins: [
-		new HtmlWebpackPlugion({
-			template: path.resolve(__dirname, '..', './src/index.html')
-		})
-	]
+  entry: path.resolve(__dirname, '..', './src/index.ts'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  optimization: {
+    emitOnErrors: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugion({
+      template: path.resolve(__dirname, '..', './public/index.html')
+    })
+  ]
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require('path')
 module.exports = {
-	mode: 'development',
-	devtool:'cheap-module-source-map', 
-	devServer:{
-		hot:true,
-		open:true,
-		port: ENV.PORT || '3000',
-		static: path.join(__dirname, 'build'),
-		watchFiles: ['src/**/*'],
-	}
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000, // ENV.PORT || '3000',
+    static: path.join(__dirname, 'build'),
+    watchFiles: ['src/**/*']
+  }
 }
