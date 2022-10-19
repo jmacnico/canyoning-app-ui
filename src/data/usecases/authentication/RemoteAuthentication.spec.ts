38036125c2f7882335c252cef1be4298ea18ad9e
micael.costa <micael.costa@celfinet.com>
2022-10-19 10:51:32 +0100

    test: ensure RemoteAuthentication calls httpPostClient with correct url

---
+++ src/data/usecases/authentication/RemoteAuthentication.spec.ts
@@ -0,0 +1,19 @@
+import { HttpPostClient } from 'data/protocols/http/HttpPostClient'
+import { RemoteAuthentication } from './RemoteAuthentication'
+
+class HttpPostClientSpy implements HttpPostClient {
+  url?: string
+  async Post (url: string): Promise<void> {
+    this.url = url
+  }
+}
+
+describe('RemoteAuthentication unit tests', () => {
+  it('Should call httpClient with correct URL', async () => {
+    const url = 'any_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.Auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
