2fc21af9106eaad9cfcad9c5e9a3f76c6cb4c4b5
micael.costa <micael.costa@celfinet.com>
2022-11-08 19:50:29 +0000

    refactor: move sut creation to a factory method

--- src/data/usecases/authentication/RemoteAuthentication.spec.ts
+++ src/data/usecases/authentication/RemoteAuthentication.spec.ts
@@ -2,11 +2,25 @@
 import { HttpPostClientSpy } from '../../test/Mock-http-clinet'
 import { RemoteAuthentication } from './RemoteAuthentication'
 
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication unit tests', () => {
   it('Should call httpClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
+
     await sut.Auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
