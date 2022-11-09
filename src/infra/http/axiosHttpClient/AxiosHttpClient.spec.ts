d3d35899b1a1e2ce36f70679ca5673e8a34bcff0
micael.costa <micael.costa@celfinet.com>
2022-11-09 09:50:17 +0000

    test: ensure AxiosHttpClient calls axions with correct body

--- src/infra/http/axiosHttpClient/AxiosHttpClient.spec.ts
+++ src/infra/http/axiosHttpClient/AxiosHttpClient.spec.ts
@@ -17,10 +17,10 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient unit tests', () => {
-  it('should call axios with correct URL and Verb', async () => {
+  it('should call axios with correct values', async () => {
     const request = mockPostRequest()
     const sut = makeSut()
     await sut.Post(request)
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
