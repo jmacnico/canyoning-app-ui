ec81f1eb86d54d55fe9904671fed30ce5c67118c
micael.costa <micael.costa@celfinet.com>
2022-11-16 21:37:41 +0000

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { render } from '@/testAdapter'
 import Login from './Login'
+import { RenderResult } from '@testing-library/react'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
 
 describe('Login unit tests', () => {
   it('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
+    const { sut } = makeSut()
+    const { getByTestId } = sut
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
