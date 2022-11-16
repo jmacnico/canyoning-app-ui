c6b99b90ae3deb131aabd90fb70b34a30d196071
micael.costa <micael.costa@celfinet.com>
2022-11-16 22:18:43 +0000

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { render } from '@/testAdapter'
 import Login from './Login'
-import { RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, RenderResult } from '@testing-library/react'
+import { IValidation } from '@/presentation/protocols/IValidation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements IValidation {
+  errorMessage: string
+  input: object
+  Validate = (input: object): string => {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Login unit tests', () => {
+  afterEach(cleanup)
+
   it('Should start with initial state', () => {
     const { sut } = makeSut()
     const { getByTestId } = sut
@@ -28,5 +43,25 @@ describe('Login unit tests', () => {
     const passwordStatus = getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
+  })
+
+  it('Should call Validation with correct email', () => {
+    const { sut, validationSpy } = makeSut()
+    const { getByTestId } = sut
+    const emailInput = getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
+  })
+
+  it('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const { getByTestId } = sut
+    const emailInput = getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
