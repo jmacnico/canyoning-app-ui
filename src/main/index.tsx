23fa058331904fe15c6e2c2b10f22d3d2d45a476
micael.costa <micael.costa@celfinet.com>
2022-11-16 15:04:05 +0000

    refactor: add index files with multiple exports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from '@/presentation/pages/login/Login'
+import { Login } from '@/presentation/pages'
 
 const element = document.getElementById('main')
 const root = createRoot(element)
