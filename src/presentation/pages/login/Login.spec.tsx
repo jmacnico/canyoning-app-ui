dd4efb8eed47c2aa700d7eb89a7362c90d36a8b9
micael.costa <micael.costa@celfinet.com>
2022-11-16 20:45:29 +0000

    test: ensure login starts with submit button disabled

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@/testAdapter'
 import Login from './Login'
 
 describe('Login unit tests', () => {
-  it('Should not render spinner and error on start', () => {
+  it('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
